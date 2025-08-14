14f7143fed2872f9bb3f214994792230a8795b18
Md Mohosin Ali <mohosinali075@gmail.com>
2025-08-14 06:08:25 +0600

    solve aws issue

--- src/app/modules/studymaterial/studymaterial.validation.ts
+++ src/app/modules/studymaterial/studymaterial.validation.ts
@@ -1,23 +1,29 @@
 import { z } from 'zod';
 
-export const StudymaterialValidations = {
-  create: z.object({
+// ✅ Create schema
+export const createStudymaterialSchema = z.object({
+  body  :z.object({
+    doc: z.string().optional(),
     name: z.string(),
     category: z.string(),
-    size: z.string(),
-    uploadDate: z.string().datetime(),
-    type: z.string(),
-    fileUrl: z.string(),
-    uploadedBy: z.string(),
-  }),
-
-  update: z.object({
-    name: z.string().optional(),
-    category: z.string().optional(),
     size: z.string().optional(),
-    uploadDate: z.string().datetime().optional(),
+    Date: z.string().datetime().optional(),
     type: z.string().optional(),
     fileUrl: z.string().optional(),
     uploadedBy: z.string().optional(),
-  }),
-};
+  })
+});
+
+// ✅ Update schema
+export const updateStudymaterialSchema = z.object({
+  body : z.object({
+    doc: z.string().optional(),
+    name: z.string().optional(),
+    category: z.string().optional(),
+    size: z.string().optional(),
+   Date: z.string().datetime().optional(),
+   type: z.string().optional(),
+   fileUrl: z.string().optional(),
+   uploadedBy: z.string().optional(),
+ })
+});
